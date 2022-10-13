08b36a6cbc1a4862b7508994534570895db25bf1
Michael Camacho <mikedev0431@gmail.com>
2022-10-12 17:10:07 -0700

    Updated with many to many relationships

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 const { dynamodb, docClient } = require('./dynamodb');
+const { v4: uuid } = require('uuid');
 
 const TABLE_NAME = 'CalendarEventApp';
-// const TABLE_NAME = 'OnlineShop';
-// const TABLE_NAME = 'OnlineShop1';
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
+/* 
+  Sample DynamodDB CRUD operations.
+  The getAllEventsByUser & getAllUsersByEvent methods will need logic to support
+  more than 100 items. We should look into what the approach would be for
+  server-side validation.
+*/
 
 app.get('/createTable', (request, response) => {
   // Create the DynamoDB service object
@@ -51,6 +53,14 @@ app.get('/createTable', (request, response) => {
         AttributeName: 'GSI2-SK',
         AttributeType: 'S'
       },
+      {
+        AttributeName: 'GSI3-PK',
+        AttributeType: 'S'
+      },
+      {
+        AttributeName: 'GSI3-SK',
+        AttributeType: 'S'
+      }
     ],
     GlobalSecondaryIndexes: [
       {
@@ -89,6 +99,31 @@ app.get('/createTable', (request, response) => {
             AttributeName: 'GSI2-SK', /* required */
             KeyType: 'RANGE' /* required */
           },
+          /* more items */
+        ],
+        Projection: { /* required */
+          NonKeyAttributes: [
+            // 'STRING_VALUE',
+            /* more items */
+          ],
+          ProjectionType: 'ALL'
+        },
+        ProvisionedThroughput: {
+          ReadCapacityUnits: 1, /* required */
+          WriteCapacityUnits: 1 /* required */
+        }
+      },
+      {
+        IndexName: 'GSI3', /* required */
+        KeySchema: [ /* required */
+          {
+            AttributeName: 'GSI3-PK', /* required */
+            KeyType: 'HASH' /* required */
+          },
+          {
+            AttributeName: 'GSI3-SK', /* required */
+            KeyType: 'RANGE' /* required */
+          }
           /* more items */
         ],
         Projection: { /* required */
@@ -153,6 +188,177 @@ app.get('/deleteTable', (request, response) => {
     });
   });
 });
+
+// Single
+app.get('/createUser', (request, response) => {
+  const UID = uuid();
+
+  const params = {
+    TableName: TABLE_NAME,
+    Item: {
+      'PK': `u#${UID}`,
+      'SK': `u#${UID}`,
+      'EntityType': 'user',
+      'Name': 'Mike Chavez'
+    }
+  };
+
+  docClient.put(params, function (err, data) {
+    if (err) return console.log("Error", err);
+
+    console.log("Success", data);
+
+    response.json({ data: 'OK' });
+  });
+});
+
+// Single
+app.get('/createEvent', (request, response) => {
+  const UID = uuid();
+
+  const params = {
+    TableName: TABLE_NAME,
+    Item: {
+      'PK': `e#${UID}`,
+      'SK': `e#${UID}`,
+      'EntityType': 'event',
+      'Name': 'TwitchCon2023'
+    }
+  };
+
+  docClient.put(params, function (err, data) {
+    if (err) return console.log("Error", err);
+
+    console.log("Success", data);
+
+    response.json({ data: 'OK' });
+  });
+});
+
+// One to One
+app.get('/createInviteeToEvent', async (request, response) => {
+  const UID = uuid();
+
+  const params = {
+    TableName: TABLE_NAME,
+    Item: {
+      'PK': 'e#135304ec-5a8b-4381-a4a9-93bd829f1035',
+      'SK': `i#${UID}`,
+      'GSI1-PK': 'u#574d271c-62d1-4f92-bc31-3d95240539be',
+      'GSI1-SK': 'u#574d271c-62d1-4f92-bc31-3d95240539be',
+      'EntityType': 'invitee'
+    }
+  };
+
+  await docClient.put(params).promise();
+
+  response.json({ data: 'OK' });
+});
+
+// Many to Many
+app.get('/getAllEventsByUser', async (request, response) => {
+  let events = [];
+
+  const params = {
+    TableName: TABLE_NAME,
+    ScanIndexForward: true,
+    IndexName: "GSI1",
+    KeyConditionExpression: "#c1490 = :c1490 And #c1491 = :c1491",
+    ExpressionAttributeValues: {
+      ":c1490": "u#37770a60-90fc-43bc-9570-474d1dd2b21a",
+      ":c1491": "u#37770a60-90fc-43bc-9570-474d1dd2b21a"
+    },
+    ExpressionAttributeNames: {
+      "#c1490": "GSI1-PK",
+      "#c1491": "GSI1-SK",
+      "#GSI1PK": "GSI1-PK",
+      "#PK": "PK",
+    },
+    ProjectionExpression: "EntityType, #GSI1PK, #PK"
+  };
+  
+  const data = await docClient.query(params).promise();
+  events = data.Items;
+
+  if (events.length === 0) return response.json({ data: [] });
+
+  const keys = events.map((event) => {
+    const the_user = {
+      'PK': event['PK'],
+      'SK': event['PK'],
+    };
+
+    return the_user;
+  });
+
+  const batchGetParams = {
+    'RequestItems': {
+      'CalendarEventApp': {
+        'Keys': keys
+      }
+    }
+  };
+
+  const batchGetResponse = await docClient.batchGet(batchGetParams).promise();
+  events = batchGetResponse.Responses.CalendarEventApp;
+
+  console.log('All events by user: ', batchGetResponse.Responses.CalendarEventApp);
+
+  response.json({ data: events });
+});
+
+// Many to Many
+app.get('/getAllUsersByEvent', async (request, response) => {
+  let users = [];
+
+  const params = {
+    TableName: TABLE_NAME,
+    ScanIndexForward: true,
+    ConsistentRead: false,
+    KeyConditionExpression: "#pk = :pk And begins_with(#sk, :sk)",
+    ExpressionAttributeNames: {
+      "#pk": "PK",
+      "#sk": "SK",
+      "#GSI1": 'GSI1-PK'
+    },
+    ExpressionAttributeValues: {
+      ":pk": "e#135304ec-5a8b-4381-a4a9-93bd829f1035",
+      ":sk": "i#"
+    },
+    ProjectionExpression: "EntityType, #pk, #sk, #GSI1"
+  };
+
+  const data = await docClient.query(params).promise();
+  users = data.Items;
+
+  const keys = users.map((user) => {
+    const the_event = {
+      'PK': user['GSI1-PK'],
+      'SK': user['GSI1-PK'],
+    };
+
+    return the_event;
+  });
+
+  const batchGetParams = {
+    'RequestItems': {
+      'CalendarEventApp': {
+        'Keys': keys
+      }
+    }
+  };
+
+  const batchGetResponse = await docClient.batchGet(batchGetParams).promise();
+  users = batchGetResponse.Responses.CalendarEventApp;
+
+  console.log('All users by event: ', users);
+
+  response.json({ data: users });
+});
+
+/* 
+  Old DynamoDB CRUD operations.
+*/
 
 app.get('/createCustomer', (request, response) => {
   var params = {
